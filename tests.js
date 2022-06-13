d30c0a3deb9f7243d6f6c2b07e6b24b7e6408bbc
Matthew Grayson <matthew.alan.grayson@gmail.com>
2022-06-13 15:05:45 -0500

    completed exercise 2

--- tests.js
+++ tests.js
@@ -25,4 +25,7 @@ describe('sayHello', function() {
     it('should be a defined function', function() {
         expect(typeof sayHello).toBe('function');
     });
+    it('should return a string', function() {
+        expect(typeof sayHello()).toBe('string');
+    });
 });
